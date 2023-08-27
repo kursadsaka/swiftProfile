85705c372aef39e7fa00870d8de83e20de9980d3
Kürşad Bumin Giray Saka <kbgs.1903@gmail.com>
2023-08-27 11:51:38 +0300

    Initial Commit

---
+++ ProfileApp/ProfileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileAppApp.swift
+//  ProfileApp
+//
+//  Created by Kürşad Saka on 27.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct ProfileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
